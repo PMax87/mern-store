1e1305db8c19cc1ade1eb1016f62e6a71d9b0dfa
PMax87 <noitulove@gmail.com>
2023-12-17 21:07:47 +0100

    ft () modify button on edit user info

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../redux";
-import { Button } from "@chakra-ui/react";
 import { CustomButton } from "../components/Commons";
+import { Button } from "@chakra-ui/react";
 
 const SettingsPage = () => {
   const user = useSelector((state: RootState) => state.userAuth.user);
@@ -16,7 +16,7 @@ const SettingsPage = () => {
         </div>
         <div className="w-4/5">
           <div className="grid grid-cols-3 gap-5">
-            <div className="p-5 border border-gray-400 flex flex-col gap-5  rounded-lg">
+            <div className="p-5 border border-gray-400 flex flex-col gap-3  rounded-lg">
               <h4 className="font-semibold">User info</h4>
               <p>
                 {user?.firstName} {user?.lastName}
@@ -24,21 +24,17 @@ const SettingsPage = () => {
               <p>{user?.number}</p>
               <p>{user?.email}</p>
               <div className="w-32">
-                <CustomButton
-                  label="Edit user info"
-                  bgColor="black"
-                  textColor="white"
-                />
+                <Button variant="outline" colorScheme="black">
+                  Edit user ingo
+                </Button>
               </div>
             </div>
             <div className="p-2 border border-black">
               <h4 className="font-semibold">Shipping address</h4>
               <div className="w-">
-                <CustomButton
-                  label="Edit shipping address"
-                  bgColor="black"
-                  textColor="white"
-                />
+                <Button variant="outline" colorScheme="black">
+                  Edit address
+                </Button>
               </div>
             </div>
           </div>
